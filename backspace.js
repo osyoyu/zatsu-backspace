c9c4a5b7d5a8835733fd0d93958a31f1db2f3de9
osyoyu <osyoyu@osyoyu.com>
2016-08-22 16:35:53 +0900

    Fix Backspace behavior

--- backspace.js
+++ backspace.js
@@ -1,3 +1,12 @@
 document.addEventListener('keydown', (e) => {
-  if (e.key === "Backspace") { history.back(); }
+  if (
+    (e.key !== "Backspace")
+    || (["INPUT", "TEXTAREA"].indexOf(document.activeElement.tagName) > -1)
+    || (document.activeElement.isContentEditable)
+  ) {
+    return;
+  }
+
+  e.preventDefault();
+  window.history.back();
 });
